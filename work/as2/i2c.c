745c882b3615c8bf379476780eb11fbf77ec0a3a
Mikhail Alexeev <mishalxv35@gmai.com>
2021-02-05 17:43:43 -0800

    made multithreading work

--- work/as2/i2c.c
+++ work/as2/i2c.c
@@ -8,30 +8,3 @@
 //Threading
 static pthread_t tid; 
 static pthread_attr_t attr; 
-
-void* TestFunct(void* arg){
-    long* limit_ptr = (long *)arg; 
-    long limit = *limit_ptr; 
-    free(limit_ptr);
-
-    long sum = 0;
-    for (int i = 0; i < limit; i ++){
-        sum += i;
-    }
-    printf("sum = %ld", sum);
-    pthread_exit(0);
-}
-
-
-void Test_threadStart(){
-    long limit = 100000;
-    pthread_attr_init(&attr); 
-    pthread_create(&tid, &attr, TestFunct, &limit); 
-
-}
-
-
-void Test_threadStop(void){
-    pthread_join(tid, NULL);
-
-}
